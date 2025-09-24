bb9fc47bb346f96a53ec5d70b25cc5daf93385fa
Caidon Buchanan <caadonbuchanan5@gmail.com>
2025-09-23 19:27:39 -0500

    Add React Frontend

---
+++ frontendemployeedatabase/frontendemployeedatabase/src/EmployeesPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EmployeesPage() {
+  return (
+    <div>
+      <h2>Employees</h2>
+      <p>Content for Employees database will go here.</p>
+    </div>
+  );
+}
+
+export default EmployeesPage;
---
+++ frontendemployeedatabase/frontendemployeedatabase/src/PayrollPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PayrollPage() {
+  return (
+    <div>
+      <h2>Payroll</h2>
+      <p>Content for Payroll database will go here.</p>
+    </div>
+  );
+}
+
+export default PayrollPage;
